c44b46b7ed3203d1b3bf0168eefec6d5857f3148
John Parker <john.parker@us.softvision.global>
2022-03-30 18:37:23 -0400

    small webhook

--- src/trydjango/pages/views.py
+++ src/trydjango/pages/views.py
@@ -74,7 +74,8 @@ def contact_view(request, *args, **kwargs):
 @require_POST
 @csrf_exempt
 def handle_github_hook(request):
-     # Verify if request came from GitHub
+
+    # Verify if request came from GitHub
     forwarded_for = u'{}'.format(request.META.get('HTTP_X_FORWARDED_FOR'))
     client_ip_address = ip_address(forwarded_for)
     whitelist = requests.get('https://api.github.com/meta').json()['hooks']
@@ -85,35 +86,49 @@ def handle_github_hook(request):
     else:
         return HttpResponseForbidden('Permission denied.')
 
-    # Verify the request signature
-    header_signature = request.META.get('HTTP_X_HUB_SIGNATURE')
-    if header_signature is None:
-        return HttpResponseForbidden('Permission denied.')
+    return HttpResponse('pong')
 
-    sha_name, signature = header_signature.split('=')
-    if sha_name != 'sha1':
-        return HttpResponseServerError('Operation not supported.', status=501)
 
-    mac = hmac.new(force_bytes(settings.GITHUB_WEBHOOK_KEY), msg=force_bytes(request.body), digestmod=sha1)
-    if not hmac.compare_digest(force_bytes(mac.hexdigest()), force_bytes(signature)):
-        return HttpResponseForbidden('Permission denied.')
+     # Verify if request came from GitHub
+    # forwarded_for = u'{}'.format(request.META.get('HTTP_X_FORWARDED_FOR'))
+    # client_ip_address = ip_address(forwarded_for)
+    # whitelist = requests.get('https://api.github.com/meta').json()['hooks']
 
-    # If request reached this point we are in a good shape
+    # for valid_ip in whitelist:
+    #     if client_ip_address in ip_network(valid_ip):
+    #         break
+    # else:
+    #     return HttpResponseForbidden('Permission denied.')
 
-    # Process the GitHub events
-    event = request.META.get('HTTP_X_GITHUB_EVENT', 'ping')
+    # # Verify the request signature
+    # header_signature = request.META.get('HTTP_X_HUB_SIGNATURE')
+    # if header_signature is None:
+    #     return HttpResponseForbidden('Permission denied.')
 
-    if event == 'ping':
-        return HttpResponse('pong')
-    elif event == 'push':
-        # Deploy some code for example
-        repo = Repo(settings.REPO_PATH)
-        origin = repo.remotes.origin
-        origin.pull('--rebase')
+    # sha_name, signature = header_signature.split('=')
+    # if sha_name != 'sha1':
+    #     return HttpResponseServerError('Operation not supported.', status=501)
 
-        commit = request.json['after'][0:6]
-        print('Repository updated with commit {}'.format(commit))
-        return HttpResponse('success')
+    # mac = hmac.new(force_bytes(settings.GITHUB_WEBHOOK_KEY), msg=force_bytes(request.body), digestmod=sha1)
+    # if not hmac.compare_digest(force_bytes(mac.hexdigest()), force_bytes(signature)):
+    #     return HttpResponseForbidden('Permission denied.')
+
+    # # If request reached this point we are in a good shape
+
+    # # Process the GitHub events
+    # event = request.META.get('HTTP_X_GITHUB_EVENT', 'ping')
+
+    # if event == 'ping':
+    #     return HttpResponse('pong')
+    # elif event == 'push':
+    #     # Deploy some code for example
+    #     repo = Repo(settings.REPO_PATH)
+    #     origin = repo.remotes.origin
+    #     origin.pull('--rebase')
+
+    #     commit = request.json['after'][0:6]
+    #     print('Repository updated with commit {}'.format(commit))
+    #     return HttpResponse('success')
 
     # In case we receive an event that's not ping or push
-    return HttpResponse(status=204)
+    #return HttpResponse(status=204)
